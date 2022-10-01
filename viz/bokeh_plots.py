5ba9e94574eb6d25c4cf2c496261693695a51729
TatevikJ <43089296+TatevikJ@users.noreply.github.com>
2022-10-01 22:07:27 +0400

    added variant description and cluster size plots

--- viz/bokeh_plots.py
+++ viz/bokeh_plots.py
@@ -1,5 +1,10 @@
 import os
+from turtle import width
+#import seaborn as sns
+import bokeh as bk
 
+import panel as pn
+import panel.widgets as pnw
 import colorlover as cl
 import matplotlib as mpl
 import matplotlib.cm as cm
@@ -13,7 +18,7 @@ from bokeh.models import HoverTool, Arrow, NormalHead, ColumnDataSource, LinearC
     PanTool, BoxZoomTool, WheelZoomTool, SaveTool, ResetTool, Div, Legend, Panel, Tabs, LabelSet
 from bokeh.plotting import figure, show
 from bokeh.transform import transform
-
+from bokeh.core.properties import value, field
 from header_html import get_div_logo, get_div_footer, get_div_title
 
 tools = [PanTool(), BoxZoomTool(), WheelZoomTool(), SaveTool(), ResetTool()]
@@ -32,11 +37,11 @@ def bokeh_composite(title, figure_list, filename, ncols=2):
     div_title = Div(text=get_div_title(title))
     save([div_logo, div_title, p], filename=filename)
 
-def bokeh_histogram(title, df, n_bins):
+def bokeh_histogram(title, df, n_bins, plot_width=800):
     hist, edges = np.histogram(df['Length'], density=False, bins=n_bins)
-    p = figure(title=title, tools='') #, background_fill_color="#fafafa"
+    p = figure(title=title, tools='') #, width=plot_width
     p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
-           fill_color="navy", line_color="white", alpha=0.5)
+           fill_color="lightblue", line_color="white")
 
     p.y_range.start = 0
     #p.legend.location = "center_right"
@@ -46,3 +51,187 @@ def bokeh_histogram(title, df, n_bins):
     #p.grid.grid_line_color="white"
 
     return p
+
+def view_alignment(title, serotype_dictionary, serotype_colors, serotype_names, include_text = False, plot_width = 800, max_num_x = 3000):
+    
+    ### seqs: Serotype list
+    ### keys: Library name
+    seqs = np.array(list(serotype_dictionary.values()))
+    keys = np.array(list(serotype_dictionary.keys()))
+    
+    N = len(seqs[0])
+    S = len(seqs)    
+    width = 1.
+    
+    tmp = [i for s in seqs for i in s]
+    colors = [serotype_colors[i] for i in tmp]
+    serotypes = [serotype_names[i] for i in tmp]
+    
+    x = np.arange(1, N + 1)
+    y = np.arange(0, S, 1)    
+    xx, yy = np.meshgrid(x, y)
+    
+    gx = xx.flatten()
+    gy = yy.flatten()
+    recty = gy + .1
+    h = 1/S
+    
+    plot_height = len(seqs)*15 + 50
+    x_range = bk.models.Range1d(0, N + 1, bounds = 'auto')
+    if N > max_num_x:
+        viewlen = max_num_x
+    else:
+        viewlen = N
+    view_range = (0, viewlen) 
+    
+    
+    
+    source = bk.models.ColumnDataSource(dict(x=gx, y=gy, recty=recty, colors = colors, serotypes = serotypes))
+    
+    p = bk.plotting.figure(title = title, 
+               plot_width = plot_width, plot_height = plot_height, 
+               x_range = view_range, y_range = keys, tools = "xwheel_zoom, xpan, reset", 
+               min_border = 0, toolbar_location = 'below')
+    
+    if include_text == True:
+        glyph = bk.models.glyphs.Text(x="x", y="y", text="text", 
+                                   text_align='center',
+                                   text_color="black",
+                                   text_font=value("monospace"))
+                                   #text_font_size=fontsize)
+    elif include_text == False:
+        glyph = bk.models.glyphs.Text(x="x", y="y", text="text", 
+                                   text_align='center',
+                                   text_color="black",
+                                   text_font=value("monospace"))
+        
+    rects = bk.models.glyphs.Rect(x = "x", y = "recty",  
+                 width = 1., height = 1., 
+                 fill_color = "colors", 
+                 line_color = None, fill_alpha = 1.)
+    
+    p.add_glyph(source, glyph)
+    p.add_glyph(source, rects)
+  
+    p.grid.visible = False
+    p.xaxis.axis_label = 'Nucleotide position'
+    p.xaxis.major_label_text_font_style = "bold"
+    p.yaxis.minor_tick_line_width = 0
+    p.yaxis.major_tick_line_width = 0
+    
+    
+    tooltips = [("serotype", "@serotypes"),]
+    p.add_tools(bk.models.HoverTool(tooltips=tooltips))
+    
+    return p
+
+def view_cluster_size_distribution(clustering_data, lower_cut = 100):
+    ### Lower and upper cluster sizes to be included in the bar plot
+    lower_cut = lower_cut
+    upper_cut = max(clustering_data["member_count"])
+
+    ### Filter the data
+    cut_tmp = clustering_data[(clustering_data.member_count > lower_cut) & 
+                            (clustering_data.member_count < upper_cut)]
+    ### Prepare the ColumnDataSource
+    ### reps is the representative names, counts is the size of the corresponding cluster
+    source = bk.models.ColumnDataSource(data = dict(reps = cut_tmp.Representative, counts = cut_tmp.member_count))
+
+    ### Make a range slider for changing the lower and upper cuts
+    range_slider = bk.models.RangeSlider(start = 1, 
+                                        end = upper_cut, 
+                                        value = (lower_cut, upper_cut), 
+                                        step=1, 
+                                        title="Range of the cluster sizes")
+
+
+    ### Make the log and linear bar plots
+    ### VV: Ideally, we should have changed the y_axis_type property, but I could now access it
+    p_linear = bk.plotting.figure(width = 1800, 
+                                height = 1300, 
+                                #y_range = [1e-2, 5e3], 
+                                x_range = source.data["reps"].values, 
+                                title="Cluster sizes", 
+                                sizing_mode="scale_both", 
+                                y_axis_type="linear")
+        
+        
+    p_linear.vbar(x = 'reps', 
+                bottom = 1e-2, 
+                top = 'counts', 
+                width = 0.7, 
+                source = source,
+                line_color = 'white', 
+                fill_color = 'lightblue')
+
+    p_log = bk.plotting.figure(width = 1800, 
+                                height = 1300, 
+                                #y_range = [1e-2, 5e3], 
+                                x_range = source.data["reps"].values, 
+                                title="Cluster sizes", 
+                                sizing_mode="scale_both", 
+                                y_axis_type="log")
+        
+        
+    p_log.vbar(x = 'reps', 
+                bottom = 1e-2, 
+                top = 'counts', 
+                width = 0.7, 
+                source = source,
+                line_color = 'white', 
+                fill_color = 'lightblue')
+
+    ### Rotate the x-axis labels
+    p_linear.xaxis.major_label_orientation = np.pi/4
+    p_log.xaxis.major_label_orientation = np.pi/4
+
+    ### Combine the log and linear plots into separate tabs of a single panel
+    panels = [bk.models.widgets.Panel(child = p_linear, title="Linear scale"), 
+            bk.models.widgets.Panel(child = p_log, title="Log scale")]
+
+    tabs = bk.models.widgets.Tabs(tabs = panels)
+
+    ### Also show the data in a table format
+        
+    columns = [
+        bk.models.TableColumn(field = 'reps', title = 'Representative'),
+        bk.models.TableColumn(field = 'counts', title = 'Cluster size')
+        ]
+
+    representative_table = bk.models.DataTable(source=source, columns=columns, fit_columns=False)
+
+
+
+    def update_data(attrname, old, new):
+
+        ### Get the current slider values
+        lower_cut = range_slider.value[0]
+        upper_cut = range_slider.value[1]
+
+        ### And introduce the cut to the data
+        cut_tmp = clustering_data[(clustering_data.member_count>lower_cut) & 
+                                (clustering_data.member_count<upper_cut)]
+        
+        ### There is a bug when the filtered data is empty
+        ### Should figure out how to correct this efficiently
+        #if not len(cut_tmp):
+        #    lower_cut = 100
+        #    upper_cut = 1000
+            
+        #    range_slider.value[0] = lower_cut
+        #    range_slider.value[1] = upper_cut
+            
+        #    cut_tmp = clustering_data[(clustering_data.member_count>lower_cut) & 
+        #                         (clustering_data.member_count<upper_cut)]
+            
+        source.data = dict(reps=cut_tmp.Representative, counts=cut_tmp.member_count)
+
+        ### Prepare new list, and update the figure
+        new_list = list(source.data["reps"].values)
+        p_log.x_range.factors = new_list
+        p_linear.x_range.factors = new_list
+
+    range_slider.on_change('value', update_data)
+    
+    return bk.layouts.column(range_slider, tabs, representative_table, width=800)
+    #return p_linear
--- viz/generate_plots.py
+++ viz/generate_plots.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
+#import seaborn as sns
 import os
 from Bio import SeqIO
 
 from header_html import write_header
-from bokeh_plots import bokeh_composite, bokeh_histogram
+from bokeh_plots import bokeh_composite, bokeh_histogram, view_alignment, view_cluster_size_distribution
 
 import sys
 
@@ -28,17 +30,44 @@ def get_df_from_fasta(path):
     df = pd.DataFrame({'Header':identifiers,'Length':lengths})
     return df
 
-def generate_main_report(config_df):
-    chimeric_orf_df = get_df_from_fasta(os.path.join(out_dir,"files", "Chimeric_ORF.fasta"))
-    enriched1_orf_df = get_df_from_fasta(os.path.join(out_dir,"files", "Enriched1_ORF.fasta"))
+def get_vd_dictionary(csv_path):
+    chimeric_library_df = pd.read_csv(csv_path, header = None)
+    ### Create a dictionary with chimeric_name:[serotype ids]
+    variant_description_dictionary = {}
+    for chimeric_library_item in chimeric_library_df[0]:
+        
+        chimeric_library_item_name = chimeric_library_item.split()[0]
+        chimeric_library_item_parents = np.array(chimeric_library_item.split()[1:]).astype("int")
+        
+        variant_description_dictionary[chimeric_library_item_name] = chimeric_library_item_parents
+    return variant_description_dictionary
 
+def get_clustering_df(clustering_data_path):
+    clustering_data = pd.read_csv(clustering_data_path)
+    ### Introduce an additional column with member count
+    clustering_data["member_count"] = clustering_data["Members_char"].str.count('AAV')
+    ### Sort the entries by the member count
+    clustering_data = clustering_data.sort_values(by=['member_count'], ascending=False)  
+    return clustering_data
+
+def generate_main_report(config_df):
+    chimeric_orf_df = get_df_from_fasta(output_paths_df.loc[output_paths_df[0] == "final_chimeric_orf_fasta"].iloc[0,1]) 
+    enriched1_orf_df = get_df_from_fasta(output_paths_df.loc[output_paths_df[0] == "final_enriched_orf_fasta"].iloc[0,1]) 
+    chimeric_lib_vd_dictionary = get_vd_dictionary(output_paths_df.loc[output_paths_df[0] == "representatives_variant_description"].iloc[0,1])
+    chimeric_lib_top20_vd_dictionary = get_vd_dictionary(output_paths_df.loc[output_paths_df[0] == "representatives_variant_description_top20_msa"].iloc[0,1])
+    chimeric_lib_clustering_info = get_clustering_df(output_paths_df.loc[output_paths_df[0] == "chimeric_lib_clustering_info"].iloc[0,1])
+
+    
 
     figure_list = []
-    figure_list += [bokeh_histogram("Chimeric library ORF length distribution", chimeric_orf_df, 50),
-                    bokeh_histogram("Enriched library ORF length distribution", enriched1_orf_df, 50)]
+    figure_list += [bokeh_histogram("Chimeric library ORF length distribution", chimeric_orf_df, 50, plot_width=600),
+                    bokeh_histogram("Enriched library ORF length distribution", enriched1_orf_df, 50, plot_width=600)]
+    figure_list += [view_alignment("Compositions of chimeric library representatives", chimeric_lib_vd_dictionary, serotype_colors, serotype_names), None]
+    figure_list += [view_alignment("Compositions of the 20 most abundant representatives in chimeric library (with multiple sequence alignment)", chimeric_lib_top20_vd_dictionary, serotype_colors, serotype_names), None]
+    figure_list += [view_cluster_size_distribution(chimeric_lib_clustering_info, lower_cut=0), None]
 
     title = config_df.loc[config_df[0] == "title_of_the_run"].iloc[0, 1]
-    bokeh_composite(title, 
+    bokeh_composite(title + "_main",  
                     figure_list, 
                     os.path.join(config_df.loc[config_df[0] == "output.dir"].iloc[0,1], "reports", title + "_main.html"), 
                     ncols=2)
@@ -47,6 +76,11 @@ if __name__ == '__main__':
     #os.chdir("c:/Users/Tatevik/Desktop/ABI/hafoe")
     config_df = pd.read_table(os.path.join(out_dir,"config", "config_file"), sep=" ", header=None)
     output_paths_df = pd.read_table(os.path.join(out_dir, "config", "output_paths"), sep=" ", header=None)
+
+    serotype_names = ["No alignment", "AAV1", "AAV2", "AAV3", "AAV4", "AAV5", "AAV6", 
+                  "AAV7", "AAV8", "AAV9", "AAV10", "AAV11", "AAV12", "AAV13", 
+                  "AAVrh8", "AAVrh10", "AAVrh32", "Multiple alignments", "gap"]
+    serotype_colors = ['#124984', '#2065ab', '#327cb7', '#4393c3', '#6bacd1', '#90c4dd', '#b1d5e7', '#d1e5f0', '#e4eef4', '#f7f6f6', '#fae9df', '#fddbc7', '#f8bfa4', '#f3a481', '#e48066', '#d6604d', '#c43b3c', '#b1182b', '#8a0b25'] #sns.color_palette("RdBu_r", len(serotype_names)).as_hex()
 
     generate_header(config_df, output_paths_df)
     generate_main_report(config_df)
